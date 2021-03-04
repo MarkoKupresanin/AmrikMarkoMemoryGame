a713044b992a285c12f90619276dd97624f4b1c6
KaptainKermit <65127786+MarkoKupresanin@users.noreply.github.com>
2021-03-04 00:33:54 -0800

    Update script.js

--- script.js
+++ script.js
@@ -24,26 +24,65 @@ function ShowChance() {
   
   var myArray = [
   // desafio means challenge
-  "¡Ai no! Muy mal, Retroceder dos lugeres.",
+  "¡Mal, muy mal! Retroceder dos lugeres.",
+  "¡Desafio! Completo Nivel Uno en dos minutos.",
   "¡Desafio! Completo Nivel Dos, en tres minutos.",
-  "¡Desafio! Completo Nivel Uno en dos minutos."
+  "¡Desafio! Completo Nivel Tres, en cuatro minutos."
 ];
   var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
 
   alert(randomItem)
 }
 
-function Starttimer(){
-    var sec = 30;
+function timer1(){
+    var sec = 120;
     var timer = setInterval(function(){
-        document.getElementById('safeTimerDisplay').innerHTML='00:'+sec;
+        document.getElementById('safeTimerDisplay1').innerHTML='Seconds Left: '+sec;
         sec--;
         if (sec < 0) {
             clearInterval(timer);
+            alert("¡Ai Caramba! Return to the game [1]")
         }
     }, 1000);
 }
 
-function EndTimer(){
-  var sec = 30;
+function endTimer1(){
+  location.reload()
+}
+
+
+
+
+function timer2(){
+    var sec = 180;
+    var timer = setInterval(function(){
+        document.getElementById('safeTimerDisplay2').innerHTML='Seconds Left: '+sec;
+        sec--;
+        if (sec < 0) {
+            clearInterval(timer);
+            alert("¡Ai Caramba! Return to the game [2]")
+        }
+    }, 1000);
+}
+
+function endTimer2(){
+  location.reload()
+}
+
+
+
+function timer3(){
+    var sec = 240;
+    var timer = setInterval(function(){
+        document.getElementById('safeTimerDisplay3').innerHTML='Seconds Left: '+sec;
+        sec--; 
+        if (sec < 0) {
+            clearInterval(timer);
+            alert("¡Ai Caramba! Return to the game [3]")
+        }
+    }, 1000);
+}
+
+function endTimer3(){
+    location.reload()
 }
